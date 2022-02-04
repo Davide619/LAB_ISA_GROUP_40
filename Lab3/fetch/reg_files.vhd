84fd206656dfe45aa414478077c8089369d1161c
kang <57479850+Kanli-cpu@users.noreply.github.com>
2022-02-04 20:39:40 +0100

    Update reg_files.vhd

--- Lab3/fetch/reg_files.vhd
+++ Lab3/fetch/reg_files.vhd
@@ -10,9 +10,9 @@ ENTITY reg_files IS
            REG1             : IN STD_LOGIC_VECTOR(4 DOWNTO 0);      -----read  register 1
            REG2             : IN STD_LOGIC_VECTOR(4 DOWNTO 0);      -----read  register 2
            WR_REG           : IN STD_LOGIC_VECTOR(4 DOWNTO 0);      -----write register 
-           DATA_IN          : IN STD_LOGIC_VECTOR(63 DOWNTO 0);     -----write data
-           DATA_OUT1        : OUT STD_LOGIC_VECTOR(63 DOWNTO 0);    -----read  data
-           DATA_OUT2        : OUT STD_LOGIC_VECTOR(63 DOWNTO 0);    -----read  data
+           INP              : IN STD_LOGIC_VECTOR(63 DOWNTO 0);     -----write data
+           OUT1             : OUT STD_LOGIC_VECTOR(63 DOWNTO 0);    -----read  data
+           OUT2             : OUT STD_LOGIC_VECTOR(63 DOWNTO 0);    -----read  data
            CLK, RST_n       : IN std_logic);
 
 END reg_files;
@@ -29,22 +29,16 @@ BEGIN
     BEGIN 
        IF RISING_EDGE(CLK) THEN
        -----READ PART
-         DATA_OUT1 <= REG(TO_INTEGER(UNSIGNED(REG1)));
-         DATA_OUT2 <= REG(TO_INTEGER(UNSIGNED(REG2)));
+         OUT1 <= REG(TO_INTEGER(UNSIGNED(REG1)));
+         OUT2 <= REG(TO_INTEGER(UNSIGNED(REG2)));
        
        ----WRITE PART
          IF RST_n = '0' then
 		REG(0 to 31) <= (others=>(others => '0'));
          ELSIF REG_WR = '1' THEN
-                REG(TO_INTEGER(UNSIGNED(WR_REG))) <= DATA_IN;
+                REG(TO_INTEGER(UNSIGNED(WR_REG))) <= INP;
          END IF;
        END IF;
    END PROCESS;
    
 END beh;
-  
-
-
-  
-    
-       
